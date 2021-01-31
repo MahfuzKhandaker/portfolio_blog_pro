23c4794932af2a7e73d7f082a47c335079818023
Mahfuz Khandaker <mahfuzkhandaker.bd@gmail.com>
2021-02-01 00:12:46 +0600

    'signup-login'

--- portfolio_blog_pro/settings.py
+++ portfolio_blog_pro/settings.py
@@ -128,3 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- portfolio_blog_pro/urls.py
+++ portfolio_blog_pro/urls.py
@@ -17,6 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
+    
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Local apps
+    path('accounts/', include('users.urls')),
+    path('', include('pages.urls')),    
 ]

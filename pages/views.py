3d2323da2296b171da7ad7b9e081ddc9fe43fd22
Mahfuz Khandaker <mahfuzkhandaker.bd@gmail.com>
2021-02-20 02:12:57 +0600

    updated

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.utils import timezone
 from blogs.models import Post
+from projects.models import Project
 from django.views.generic import TemplateView
 from django.db.models import Count
 
@@ -15,8 +16,9 @@ class HomePageView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
-        context['featured_posts'] = Post.published.filter(featured=True)[:3]
+        context['featured_posts'] = Post.published.filter(featured=True)[:6]
         context['post_num'] = Post.published.count()
+        context['project_num'] = Project.objects.count()
         context['most_recent'] = Post.published.all().order_by('-timestamp', '-updated')[:3]
         context['post_by_category_count']  = Post.published.values('categories__title').annotate(Count('categories__title')).order_by('categories')
         return context

5c13ad24a8f342928be881191065cb4a855fd6a8
Mahfuz Khandaker <mahfuzkhandaker.bd@gmail.com>
2021-04-29 03:19:45 +0600

    styling

--- pages/views.py
+++ pages/views.py
@@ -5,7 +5,7 @@ from blogs.models import Post, Category
 from projects.models import Project
 from django.views.generic import TemplateView
 from django.db.models import Count
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.template.loader import render_to_string
 from django.http import JsonResponse
 from django.core import serializers
@@ -18,24 +18,23 @@ class HomePageView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
         context['featured_posts'] = Post.published.filter(featured=True)[:2]
-        paginator = Paginator(context['featured_posts'], 2)
-        page = self.request.GET.get('page')
-        try:
-            context['featured_posts'] = paginator.page(page)
-        except PageNotAnInteger:
-            context['featured_posts'] = paginator.page(1)
-        except EmptyPage:
-            context['featured_posts'] = paginator.page(paginator.num_pages)
-
+        # paginator = Paginator(context['featured_posts'], 2)
+        # page = self.request.GET.get('page')
+        # try:
+        #     context['featured_posts'] = paginator.page(page)
+        # except PageNotAnInteger:
+        #     context['featured_posts'] = paginator.page(1)
+        # except EmptyPage:
+        #     context['featured_posts'] = paginator.page(paginator.num_pages)
         context['post_num'] = Post.published.count()
         context['project_num'] = Project.objects.count()
-        context['most_recent'] = Post.published.all().order_by('-timestamp', '-updated')[:3]
+        context['most_recent'] = Post.published.all().order_by('-timestamp', '-updated')[:2]
         context['post_by_category_count']  = Post.published.values('category__title').annotate(Count('category__title')).order_by('category')        
 
-        if self.request.is_ajax():
-            html = render_to_string('pagination.html', context, request=self.request)
-            featured_posts = serializers.serialize('json', list(featured_posts))
-            return JsonResponse({'form': html, 'featured_posts': featured_posts})
+        # if self.request.is_ajax():
+        #     html = render_to_string('pagination.html', context, request=self.request)
+        #     featured_posts = serializers.serialize('json', list(featured_posts))
+        #     return JsonResponse({'form': html, 'featured_posts': featured_posts})
 
         return context
 

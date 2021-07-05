5b6d215318380f6bb071f33455e7b93e3e2aa721
Mahfuz Khandaker <mahfuzkhandaker.bd@gmail.com>
2021-07-06 00:06:58 +0600

    deploy and run

--- portfolio_blog_pro/urls.py
+++ portfolio_blog_pro/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # Django admin
-    path('admin-at-letsflyonthewebsky/', admin.site.urls),
+    path('admin-at-webdevsky/', admin.site.urls),
     
     # User management
     path('accounts/', include('allauth.urls')),
